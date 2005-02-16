2e57ac5241bd3eb08ada286eb46ce96e1958aa43
Richard Clamp <richardc@unixbeard.net>
2005-02-16 12:41:29 +0000

    Import of RCLAMP/Mail-Thread-Arc-0.22 from CPAN.
    gitpan-cpan-distribution: Mail-Thread-Arc
    gitpan-cpan-version:      0.22
    gitpan-cpan-path:         RCLAMP/Mail-Thread-Arc-0.22.tar.gz
    gitpan-cpan-author:       RCLAMP
    gitpan-cpan-maturity:     released

--- lib/Mail/Thread/Arc.pm
+++ lib/Mail/Thread/Arc.pm
@@ -3,9 +3,10 @@ package Mail::Thread::Arc;
 use SVG;
 use Date::Parse qw( str2time );
 use base qw( Class::Accessor::Chained::Fast );
-__PACKAGE__->mk_accessors(qw( messages width height svg ));
+__PACKAGE__->mk_accessors(qw( messages message_offsets selected_message
+                              width height svg ));
 
-our $VERSION = '0.21';
+our $VERSION = '0.22';
 
 =head1 NAME
 
@@ -64,15 +65,19 @@ sub render {
         $self->date_of( $a ) <=> $self->date_of( $b )
     } @messages;
 
+    $self->messages( \@messages );
+
     $self->width( ( @messages + 1 ) * $self->message_radius * 3 );
     $self->height( $self->maximum_arc_height * 2 + $self->message_radius * 6 );
-    $self->svg( SVG->new( width => $self->width, height => $self->height ) );
+    $self->svg( SVG->new( width => $self->width, height => $self->height, onload => "init(evt)" ) );
 
     {
         # assign the numbers needed to compute X
         my $i;
-        $self->messages( { map { $_ => ++$i } @messages } );
+        $self->message_offsets( { map { $_ => ++$i } @messages } );
     }
+    $self->svg->script->CDATA( $self->javascript_chunk );
+
     $self->draw_arc( $_->parent, $_ ) for @messages;
     $self->draw_message( $_ ) for @messages;
 
@@ -89,16 +94,45 @@ Draw the message on the SVG canvas.
 sub draw_message {
     my ($self, $message) = @_;
 
-    my $group = $self->svg->group;
+    my $i = $self->message_offsets->{$message} - 1;
+
+    my $link = $self->make_link($message);
+    my $root = $self->svg;
+    $root = $root->anchor( -href=> $link ) if defined $link;
+
+    my $group = $root->group(
+        id          => "road$i",
+        onmouseover => "set_group_color( $i, 'blue' )",
+        $self->message_style( $message ),
+       );
+
     $group->title->cdata( $message->header('from') );
     $group->desc->cdata( "Date: " . $message->header('date') );
+
     $group->circle(
         cx => $self->message_x( $message ),
         cy => $self->message_y,
         r  => $self->message_radius,
-        style => $self->message_style( $message ),
        );
+    
 }
+
+
+=head2 make_link( $message )
+
+Return an URI based on the message. By default returns 
+undef meaning that that the message is not a link.
+
+However if this module is subclassed then a meaningful
+URI can be returned where appropriate.
+
+=cut
+
+sub make_link {
+    my ($self, $message) = @_;
+    return;
+}
+
 
 =head2 draw_arc( $from, $to )
 
@@ -118,6 +152,9 @@ sub draw_arc {
     my $x = $self->message_x( $from );
     my $y = $self->message_y + ( $top ? -$self->message_radius : $self->message_radius);
 
+    my %offsets = %{ $self->message_offsets };
+
+    my $path;
     if ($radius > $self->maximum_arc_height) { # uh oh - trickyness
         my $max = $self->maximum_arc_height;
         # to Y - the relative part of the first curve
@@ -126,22 +163,22 @@ sub draw_arc {
         my $x2 = $self->message_x( $to ) - $max;
         my $y2 = $y + $toy;
 
-        $self->svg->path(
-            d => join(' ',
-                      "M $x,$y",                        #start the path
-                      "a$max,$max 0 0,$top $max,$toy",  # arc up
-                      "L $x2,$y2",                      # line across
-                      "a$max,$max 0 0,$top $max,$toy2", # arc down
-                     ),
-            style => $self->arc_style( $from, $to ),
-           );
+        $path = join(' ',
+                     "M $x,$y",                        #start the path
+                     "a$max,$max 0 0,$top $max,$toy",  # arc up
+                     "L $x2,$y2",                      # line across
+                     "a$max,$max 0 0,$top $max,$toy2", # arc down
+                    );
     }
     else {
-        $self->svg->path(
-            d => "M $x,$y a$radius,$radius 0 1,$top $distance,0",
-            style => $self->arc_style( $from, $to ),
-           );
+        $path = "M $x,$y a$radius,$radius 0 1,$top $distance,0";
     }
+
+    my $group = $self->svg->group(
+        id => "road$offsets{ $from }-$offsets{ $to }",
+        $self->arc_style( $from, $to )
+       );
+    $group->path( d  => $path );
 }
 
 =head2 message_radius
@@ -162,11 +199,11 @@ Returns the style hash for the message circle.
 sub message_style {
     my ($self, $message) = @_;
 
-    return +{
+    return (
         stroke         => 'red',
         fill           => 'white',
         'stroke-width' => $self->message_radius / 4,
-    };
+    );
 }
 
 =head2 maximum_arc_height
@@ -188,11 +225,12 @@ Returns the style hash for the connecting arc,
 
 sub arc_style {
     my ($self, $from, $to) = @_;
-    return {
-        fill   => 'none',
+
+    return (
         stroke => 'black',
+        fill   => 'none',
         'stroke-width' => $self->message_radius / 4,
-    }
+    );
 }
 
 =head2 message_x( $container )
@@ -203,7 +241,7 @@ returns the X co-ordinate for a message
 
 sub message_x {
     my ($self, $message) = @_;
-    return $self->messages->{ $message } * $self->message_radius * 3;
+    return $self->message_offsets->{ $message } * $self->message_radius * 3;
 }
 
 =head2 message_y
@@ -245,6 +283,78 @@ The date the message was sent, in epoch seconds
 sub date_of {
     my ($self, $container) = @_;
     return str2time $container->header( 'date' );
+}
+
+sub javascript_chunk {
+    my $self = shift;
+    my $js = <<'END';
+/* lifted from http://roasp.com/tutorial/tutorial5.shtml */
+var SVGDoc;
+var groups = new Array();
+var last_group;
+
+function init(e) {
+    SVGDoc = e.getTarget().getOwnerDocument();
+END
+
+    # just what groups do we manage?
+    my %offset = %{ $self->message_offsets };
+    for my $id (sort { $a <=> $b } values %offset) {
+        my $group = $id - 1;
+        my $message = $self->messages->[$group];
+        my @path = $id - 1;
+        while ($message) {
+            last unless $message->parent;
+            push @path, $offset{ $message->parent } - 1,
+              "'$offset{ $message->parent }-$offset{ $message }'";
+            $message = $message->parent;
+        }
+
+        $js .= "    append_group( ". join(', ', @path ) . " ); // road $group\n"
+    }
+
+    if (my $m = $self->selected_message) {
+        my $group = $offset{ $m } - 1;
+        $js .= "    set_group_color( $group, 'blue' ); // select group $group\n"
+    }
+
+    $js .= <<'END';
+}
+
+function append_group() {
+    var roads = new Array();
+    for (var i = 0; i < arguments.length; i++) {
+        var index = arguments[i];
+        var road  = SVGDoc.getElementById("road" + index);
+        roads[roads.length] = road;
+    }
+    groups[groups.length] = roads;
+}
+
+function set_group_color(group_index, color) {
+    if ( last_group != null ) {
+        _set_group_color(last_group, "black");
+    }
+    _set_group_color(group_index, color);
+    last_group = group_index;
+}
+
+function _set_group_color(group_index, color) {
+    var roads = groups[group_index];
+
+    for (var i = 0; i < roads.length; i++) {
+         var road = roads[i];
+
+         //alert( "Setting " + road + " to " + color );
+         // set the color
+         road.setAttribute("stroke", color);
+         // pop to top - seems to blow errors on circles
+         // road.getParentNode.appendChild(road);
+    }
+}
+END
+
+    return $js;
 }
 
 
